d4ede0afffc8510caf61c20e107a77bd3ef84298
Ben <villauben@gmail.com>
2023-04-01 02:59:56 +0200

    disabled elytra boost via fireworkrockets

---
+++ src/main/java/net/ben_kenobi/progression_revamp/mixin/ElytraBoostDisabler.java
@@ -0,0 +1,23 @@
+package net.ben_kenobi.progression_revamp.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.FireworkRocketItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+@Mixin(FireworkRocketItem.class)
+public class ElytraBoostDisabler {
+    @Inject(method = "use", at = @At("HEAD"), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
+    public void changeB(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
+        cir.setReturnValue(TypedActionResult.pass(user.getStackInHand(hand)));
+        cir.cancel();
+    }
+}
