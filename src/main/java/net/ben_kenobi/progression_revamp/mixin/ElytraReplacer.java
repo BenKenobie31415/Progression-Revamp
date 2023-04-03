94d2ac4ebd39d316ce353d068db7e6d95019939a
Ben <villauben@gmail.com>
2023-04-03 23:25:05 +0200

    edited loot tables

--- src/main/java/net/ben_kenobi/progression_revamp/mixin/ElytraReplacer.java
+++ src/main/java/net/ben_kenobi/progression_revamp/mixin/ElytraReplacer.java
@@ -8,11 +8,11 @@ import net.ben_kenobi.progression_revamp.item.ModItems;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.structure.EndCityGenerator;
 
-@Mixin(EndCityGenerator.Piece.class)
+/*@Mixin(EndCityGenerator.Piece.class)
 public abstract class ElytraReplacer {
     @ModifyArg(method = "handleMetadata",
         at = @At( value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;<init>(Lnet/minecraft/item/ItemConvertible;)V", ordinal = 0), index = 0)
     public ItemConvertible changeItem(ItemConvertible item) {
         return ModItems.BROKEN_SWORD;
     }
-}
+}*/
